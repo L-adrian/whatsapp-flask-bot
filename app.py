32a37d4783d350bf87a1abff8f4039875e523308
L-adrian <purplemangoadrian@gmail.com>
2025-04-24 10:12:50 -0400

    Tercer Commit

--- app.py
+++ app.py
@@ -52,4 +52,5 @@ def enviar_mensaje(to, message):
 
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Usa el puerto de Render o 5000 local
+    app.run(host='0.0.0.0', port=port)
